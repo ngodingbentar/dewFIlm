28b61ab718eea21daa3a48f25747d070ea76c097
dewaqintoro97 <dewa.qintoro@students.amikom.ac.id>
2020-04-19 23:06:15 +0700

    edit card v1

--- src/script/component/app-bar.js
+++ src/script/component/app-bar.js
@@ -11,7 +11,74 @@ class AppBar extends HTMLElement {
 
     render() {
         this.shadowDOM.innerHTML = `
-       <h2 class="text-uppercase">DewFilm</h2>`;
+        <style>
+        .topnav {
+            position: relative;
+            overflow: hidden;
+            background-color: #333;
+            height: 50px;
+          }
+          
+          .topnav a {
+            float: left;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
+          }
+          
+          .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+          }
+          
+          .topnav a.active {
+            background-color: #0a3d62;
+            color: white;
+          }
+          
+          .topnav-centered a {
+            float: none;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+          }
+          
+          .topnav-right {
+            float: right;
+          }
+          
+          /* Responsive navigation menu (for mobile devices) */
+          @media screen and (max-width: 600px) {
+            .topnav a, .topnav-right {
+              float: none;
+              display: block;
+            }
+            
+            .topnav-centered a {
+              position: relative;
+              top: 0;
+              left: 0;
+              transform: none;
+            }
+          }
+          
+          .h1a{
+            color: #acc3d6;
+          }
+          
+          input{
+            background-color: #6a89cc;
+          }
+        </style>
+        <div class="topnav">
+            <div class="topnav-centered">
+            <a href="index.html" class="active">dewFilm</a>
+            </div>
+        </div>
+        `;
     }
 }
 
--- src/script/component/club-list.js
+++ src/script/component/club-list.js
@@ -15,19 +15,58 @@ class ClubItem extends HTMLElement {
 
     render() {
         this.shadowDOM.innerHTML = `
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+       
+        <style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+/* Float four columns side by side */
+.column {
+  float: left;
+  width: 25%;
+  padding: 0 10px;
+}
+
+/* Remove extra left and right margins, due to padding */
+.row {margin: 0 -5px;}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive columns */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+    display: block;
+    margin-bottom: 20px;
+  }
+}
+
+/* Style the counter cards */
+.card {
+    max-width: 200px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 16px;
+  text-align: center;
+  background-color: #f1f1f1;
+}
+</style>
         
-        
-        
-        <div class="col-sm-3">
-            <div class="card text-white bg-secondary mb-3">
+        <div class="">
+            <div class="">
+                <h3>${this._club.Title}</h3>
                 <img src="${this._club.Poster}" class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">${this._club.Title}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">${this._club.Year}</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <button type="button" class="btn btn-primary see-detail btn-hover" data-toggle="modal" data-target="#exampleModalScrollable" data-id="${this._club.Type}">Selengkapnya</button>
-                </div>
+                <p></p>
+                <p>${this._club.Year}</p>
             </div>
         </div>
         
@@ -54,6 +93,7 @@ class ClubList extends HTMLElement {
         this.shadowDOM.innerHTML = "";
         this._clubs.forEach(club => {
             const clubItemElement = document.createElement("club-item");
+            clubItemElement.style = ('background-color:grey;padding:9px;margin:6px;align-content: center;'); 
             clubItemElement.club = club;
             this.shadowDOM.appendChild(clubItemElement);
         })
