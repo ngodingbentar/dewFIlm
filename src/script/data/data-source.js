873129119878b1366a0f24dde18fdb6b073a9856
ngodingbentar <dewaqintoro@gmail.com>
2023-11-02 22:05:58 +0700

    updated: endpoint

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,6 +1,7 @@
 class DataSource {
     static searchClub(keyword) {
-        return fetch(`http://www.omdbapi.com/?apikey=2567cbc3&s=${keyword}`)
+        // return fetch(`http://www.omdbapi.com/?apikey=2567cbc3&s=${keyword}`)
+        return fetch(`https://ngodingbentar-be-v3.vercel.app/api/v1/film?s=${keyword}`)
         .then(response => {
             return response.json();
         })
